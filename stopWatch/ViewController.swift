2f5cba90ebec213f37148fbcf6e54b0b90ee4bd0
Chris Howie <chris.howie@skookum.com>
2015-09-08 21:00:02 -0400

    stopWatch

---
+++ stopWatch/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  stopWatch
+//
+//  Created by Chris Howie on 9/8/15.
+//  Copyright © 2015 com.chrishowie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var timer = NSTimer()
+    
+    var time = 0
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    func increaseTimer() {
+        time++
+        
+        timerLabel.text = String(time)
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+    }
+    
+    @IBAction func reset(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+        time = 0
+        timerLabel.text = "0"
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
